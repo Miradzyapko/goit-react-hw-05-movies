ba6a0c6a1ccb54e962aa9bba3a7b32d08494a31a
Miradzyapko <95940989+Miradzyapko@users.noreply.github.com>
2023-05-27 21:31:33 +0000

    Deploying to gh-pages from @ Miradzyapko/goit-react-hw-05-movies@f3c009d93edf291de0ec3cbc3a0dd9deefe548dc 🚀

--- src/components/servisesAPI/API.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const MOVIES_API_KEY = '9386514e4340ac496b95fab8e8f53d01';
-const URL = 'https://api.themoviedb.org/3/';
-export const getTrendingMovies = () => {
- 
-  return axios.get(`${URL}movie/550?api_key=${MOVIES_API_KEY}`);
-
-};
-
